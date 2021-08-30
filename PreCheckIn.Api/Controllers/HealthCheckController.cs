d252457273cf9d16dc3df52b538cfa9c321d9767
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2021-08-30 13:54:48 +0100

    - add HealthCheckController

---
+++ PreCheckIn.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreCheckIn.Api.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    [Route("/")]
+    public class HealthCheckController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
--- PreCheckIn.Api/Startup.cs
+++ PreCheckIn.Api/Startup.cs
@@ -62,8 +62,10 @@ namespace PreCheckIn.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+               
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PreCheckIn.Api v1"));
+               
             }
 
             app.UseHttpsRedirection();
